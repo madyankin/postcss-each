757ac6af99909c574a8479fd25bab900c6459723
Alexander Madyankin <alexander@madyankin.name>
2015-05-19 07:38:51 +0600

    Clean the code up. Fixes #1

--- index.js
+++ index.js
@@ -4,13 +4,9 @@ var list    = postcss.list;
 module.exports = postcss.plugin('postcss-each', function(opts) {
   opts = opts || {};
 
-  function error(rule, message) {
-    throw rule.error(message, { plugin: 'postcss-each' });
-  }
-
   function checkParams(rule) {
     if (rule.params.indexOf('in') == -1) {
-      error(rule, 'Missed "in" keyword in @each');
+      throw rule.error('Missed "in" keyword in @each');
     }
 
     var params  = rule.params.split('in');
@@ -18,11 +14,11 @@ module.exports = postcss.plugin('postcss-each', function(opts) {
     var values  = params[1].trim();
 
     if (!name.match(/\$[_a-zA-Z]\w+/)) {
-      error(rule, 'Missed variable name in @each');
+      throw rule.error('Missed variable name in @each');
     }
 
     if (!values.match(/(\w+\,?\s?)+/)) {
-      error(rule, 'Missed values list in @each');
+      throw rule.error('Missed values list in @each');
     }
   }
 
